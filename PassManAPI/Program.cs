4784f0e1fdba0767ec4f8cd67bd7a93807acbb27
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2025-12-08 13:28:15 +0100

    Created SqlTest controller and call, added health and depends in docker-compose.yml

--- PassManAPI/Program.cs
+++ PassManAPI/Program.cs
@@ -2,7 +2,7 @@ namespace PassManAPI;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +15,14 @@ public class Program
             options.IncludeXmlComments(System.IO.Path.Combine(AppContext.BaseDirectory, xmlFilename));
         });
         var app = builder.Build();
+
+        // Call DB to test the connectivity
+        if (app.Environment.IsDevelopment())
+        {
+            var conn = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? "Server=db;Port=3306;Database=passManDB;User=root;Password=hihi";
+            await PassManAPI.Controllers.SqlTest.RunAsync(conn);
+        }
 
         // Enable swagger UI in development environment
         if (app.Environment.IsDevelopment())
