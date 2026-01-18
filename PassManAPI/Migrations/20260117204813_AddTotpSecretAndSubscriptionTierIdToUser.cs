f6224a79937c0a93fa7b25bdab5baaa8dc76e07c
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2026-01-18 01:24:03 +0100

    Removed completely optional migration for altering the precision of timestamps that causes issues on  some sql servers

--- PassManAPI/Migrations/20260117204813_AddTotpSecretAndSubscriptionTierIdToUser.cs
+++ PassManAPI/Migrations/20260117204813_AddTotpSecretAndSubscriptionTierIdToUser.cs
@@ -11,46 +11,6 @@ namespace PassManAPI.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedAt",
-                table: "Vaults",
-                type: "datetime(6)",
-                nullable: false,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "datetime(6)",
-                oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedAt",
-                table: "Credentials",
-                type: "datetime(6)",
-                nullable: false,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "datetime(6)",
-                oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "Timestamp",
-                table: "AuditLogs",
-                type: "datetime(6)",
-                nullable: false,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "datetime(6)",
-                oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "CreatedAt",
-                table: "AspNetUsers",
-                type: "datetime(6)",
-                nullable: false,
-                defaultValueSql: "CURRENT_TIMESTAMP",
-                oldClrType: typeof(DateTime),
-                oldType: "datetime(6)",
-                oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
-
             migrationBuilder.AddColumn<Guid>(
                 name: "SubscriptionTierId",
                 table: "AspNetUsers",
