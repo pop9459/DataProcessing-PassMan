37f30197b23fc4f22013481fc27282cbccf3bc0c
CosminBuzicUni <cosmin.buzic@student.nhlstenden.com>
2026-01-18 03:25:48 +0100

    fixed missing tag table in 20260117201129

--- PassManAPI/Migrations/20260117201129_AddTagsAndCredentialTags.cs
+++ PassManAPI/Migrations/20260117201129_AddTagsAndCredentialTags.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 #nullable disable
 
@@ -50,11 +51,68 @@ namespace PassManAPI.Migrations
                 oldClrType: typeof(DateTime),
                 oldType: "datetime(6)",
                 oldDefaultValueSql: "CURRENT_TIMESTAMP(6)");
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CredentialTags",
+                columns: table => new
+                {
+                    CredentialId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CredentialTags", x => new { x.CredentialId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_CredentialTags_Credentials_CredentialId",
+                        column: x => x.CredentialId,
+                        principalTable: "Credentials",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CredentialTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CredentialTags_TagId",
+                table: "CredentialTags",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "CredentialTags");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
             migrationBuilder.AlterColumn<DateTime>(
                 name: "CreatedAt",
                 table: "Vaults",
